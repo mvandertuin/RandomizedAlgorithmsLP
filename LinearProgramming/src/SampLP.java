011806f64b0a3d99db30ebc1842e96979f3b8130
kjgm <kjgm_vd_linden@hotmail.com>
2015-02-23 12:57:27 +0100

    Comments added to SampLP

--- LinearProgramming/src/SampLP.java
+++ LinearProgramming/src/SampLP.java
@@ -10,24 +10,27 @@ public class SampLP {
 	
 	public double[] solve() {
 		List<Constraint> S = new ArrayList<Constraint>();
-		int n = lp.getH().size();
-		int d = lp.getC().length;
+		int n = lp.getH().size(); //Number of constraints
+		int d = lp.getC().length; //Number of variables
 		if(n < (9*d^2)){
 			return new SimplexSolver(lp).solve();
 		} 
-		List<Constraint> V = new ArrayList<Constraint>();
-		V.addAll(lp.getH());
+		List<Constraint> V = new ArrayList<Constraint>(lp.getH());
 		double[] x;
 		while(V.size()>0){
+			//r = min{ d sqrt(n), |H\S|}
 			int r = (int) Math.round(Math.min(d*Math.sqrt(n), (double) (lp.getH().size() - S.size())));
 			ArrayList<Constraint> RandS = chooseR(lp.getH(), S, r);
 			RandS.addAll(S);
+			//Recursive call on the instance with constraint set R U S
 			x = new SampLP(new LPInstance(RandS, lp.getC())).solve();
-			V = new ArrayList<Constraint>();
+			//V <- {all vertices in H that are violated by the values of x}
+			V.clear();
 			for(Constraint c: lp.getH()){
 				if(!c.check(x))
 					V.add(c);
 			}
+			//if |V| <= 2sqrt(n) then S <- S U V
 			if(V.size() <= 2 * Math.sqrt(n))
 				for(Constraint c: V)
 					if(!S.contains(c))
@@ -36,10 +39,10 @@ public class SampLP {
 		return x;
 	}
 	
+	//Choose rSize constraints randomly from the set H\S
 	private static ArrayList<Constraint> chooseR(List<Constraint> H, List<Constraint> S, int rSize){
 		ArrayList<Constraint> R = new ArrayList<Constraint>();
-		List<Constraint> pool = new ArrayList<Constraint>();
-		pool.addAll(H);
+		List<Constraint> pool = new ArrayList<Constraint>(H);
 		pool.removeAll(S);
 		while(R.size() < rSize){
 			Constraint c = pool.get((int) Math.random()*pool.size()); 
