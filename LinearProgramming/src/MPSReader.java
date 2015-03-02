cda33b7f6a4953b528bc9b008979ca5e4a6ecfd9
Marieke van der Tuin <msvandertuin@gmail.com>
2015-03-02 16:41:27 +0100

    Added miplib tests which succeed

--- LinearProgramming/src/MPSReader.java
+++ LinearProgramming/src/MPSReader.java
@@ -1,11 +1,9 @@
 
 
 import java.io.File;
+import java.io.PrintWriter;
 import java.net.URI;
 import java.util.ArrayList;
-
-import cern.colt.Arrays;
-import cern.colt.matrix.DoubleMatrix1D;
 
 import com.joptimizer.optimizers.LPStandardConverter;
 import com.joptimizer.util.MPSParser;
@@ -13,17 +11,45 @@ import com.joptimizer.util.MPSParser;
 public class MPSReader {
 
 	public static void main(String[] args){
-		MPSParser m = new MPSParser();
-		LPStandardConverter lps = new LPStandardConverter();
+		
 		try {			
-			//file should be present in bin folder
-			m.parse("mps_files/atlanta-ip.mps");
-			lps.toStandardForm(m.getC(), m.getG(), m.getH(), m.getA(), m.getB(), m.getLb(), m.getUb());
-			LPInstance lpi = transformToLPInstance(lps);
-			double[] x = new SimplexSolver(lpi).solve();
-			System.out.println(Arrays.toString(x));
-			double[] y = new SampLP(lpi).solve();
-			System.out.println(Arrays.toString(y));
+	        PrintWriter printWriter = new PrintWriter(new File("MIPLIB_test.txt"));
+	        printWriter.write("problemName foundObjective \n");
+
+			File folder = new File(new URI(Thread.currentThread().getContextClassLoader().getResource("mps_files/miplib_complete").toString()));
+
+	        File[] listOfFiles = folder.listFiles();
+	        for (File file : listOfFiles) {
+	            if (file.isFile()) {
+	            	try{
+	            		System.out.println(file.getName());
+	            		MPSParser m = new MPSParser();
+	            		LPStandardConverter lps = new LPStandardConverter();
+	            		m.parse(file);
+	            		lps.toStandardForm(m.getC(), m.getG(), m.getH(), m.getA(), m.getB(), m.getLb(), m.getUb());
+	    				LPInstance lpi = transformToLPInstance(lps);
+	    				SimplexSolver ss = new SimplexSolver(lpi);
+	    				ss.solve();
+		                printWriter.write(file.getName() + " " + ss.getSolution() + "\n");
+		                printWriter.flush();			
+
+	    			}
+	    			catch(OutOfMemoryError e){ 
+		                printWriter.write(file.getName() + " " + "heapSpace" + "\n");
+		                printWriter.flush();	
+	    			}
+	            	catch(IllegalArgumentException e){
+		                printWriter.write(file.getName() + " " + "tooLarge" + "\n");
+		                printWriter.flush();			
+	            	}
+	            	catch(Exception e){
+		                printWriter.write(file.getName() + " " + "someOtherError" + "\n");
+		                printWriter.flush();			
+	            	}
+
+	            }
+	        }
+	        printWriter.close();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
